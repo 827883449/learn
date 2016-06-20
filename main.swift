502fca9f0edab6cf76bbe672509b9b229c5926ce
827883449 <827883449@qq.com>
2016-06-20 10:47:50 +0800

    从1加到100
    从1加到100，基本思路就是两个数相加，然后使用循环语句并判断其条件使1到100相加

---
+++ main.swift
@@ -0,0 +1,18 @@
+//
+//  main.swift
+//  learn
+//
+//  Created by 刘骁汉 on 16/6/20.
+//  Copyright © 2016年 刘骁汉. All rights reserved.
+//
+
+import Foundation
+var a=0
+var b=0
+while a < 100
+{a++
+    b=a+b
+}
+
+print(b)
+
